12b1e1c06b03b59652fe9e837d4a5fb75e76bfb1
Elias Sanchez Martin <eliiasxxm@gmail.com>
2022-12-19 22:54:02 +0100

    Final

--- static/app.js
+++ static/app.js
@@ -1,3 +1,8 @@
-// Elías Sánchez Martín
-//
-// Completar JS aqui
+window.addEventListener("load", function () {
+    document.querySelector('button.new_client').addEventListener('click', function () {
+        document.querySelector('#new_client').classList.remove('display_none');
+    });
+    document.querySelector('button.close').addEventListener('click', function () {
+        document.querySelector('#new_client').classList.add('display_none');
+    });
+});
